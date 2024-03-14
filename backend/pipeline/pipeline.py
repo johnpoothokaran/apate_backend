028e88407372805d9c2de7970d0857bba4c17556
John Poothokaran <johnmp@alumni.stanford.edu>
2024-03-14 19:06:19 -0400

    Created Pipeline class with outline of full pipeline. Runs in main

--- backend/pipeline/pipeline.py
+++ backend/pipeline/pipeline.py
@@ -5,3 +5,31 @@ Output  - JSON response for client
 
 
 '''
+
+from utils import get_keys
+
+
+class Pipeline():
+
+    def __init__(self):
+        
+        #self.query = None
+        #self.response = None
+        self.keychain = get_keys.read_keys_from_json()
+        print('Successful creation of Pipeline object')
+        
+    
+    def run_pipeline(self, query: str):
+        
+        print('\nUSER QUERY:',query)
+        
+        #print('\n',self.keychain,'\n')
+
+        # runs preprocess functions
+
+        # runs LLM ops functions
+
+        # runs postprocess functions
+
+        response = 'Maybe next time...\n\n'
+        print('RESPONSE:',response)
--- main.py
+++ main.py
@@ -7,3 +7,17 @@ Runs the backend processes
 Manages the API
 
 '''
+
+from utils import get_keys
+from backend.pipeline import pipeline
+#from pipeline import Pipeline
+
+def main():
+
+    test_run = pipeline.Pipeline()
+    test_run.run_pipeline("I'm the test query")
+
+    return None
+
+if __name__ == "__main__":
+    main()
